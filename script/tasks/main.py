0212e372e457fe60ebdb8f4fecf53dc698c55b47
Richard King <richrdkng@gmail.com>
2016-06-14 15:00:38 +0200

    Add phpunit.xml with initial unit test configurations
    - update main.py task according to phpunit.xml
    - clean up main.py a bit

--- script/tasks/main.py
+++ script/tasks/main.py
@@ -8,16 +8,13 @@ args = sys.argv[1:]
 
 if len(args) > 0:
     if args[0] == "test":
+        options = "--configuration test/phpunit.xml"
+        tests = "test/cases/."
         filters = ""
 
         if len(args) > 2:
             if args[1] == "--filter":
                 filters = "--filter {test}".format(test=args[2])
-
-        options = "--bootstrap test/bootstrap.php " \
-                  "--colors=always"
-
-        tests = "test/cases/."
 
         Run.call("php vendor/bin/phpunit {options} {filters} {tests}".format(options=options,
                                                                              filters=filters,
